0d9c488141955654c421248fddef24c53ebb4d28
omarmm <eng.omarmm@gmail.com>
2015-10-06 14:18:10 +0200

    some edits on style and text

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,8 +19,10 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
 </head>
 <body>
+
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -30,11 +32,11 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="http://tutacare.github.io/tutapos/">omarmm</a>
+				<a class="navbar-brand" href="#" style="font-size:22px; font-weight:bold; color:#cff899;">Style</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
+				<ul class="nav navbar-nav" style="font-size:20px; ">
 					<li><a href="{{ url('/') }}">{{trans('menu.dashboard')}}</a></li>
 					@if (Auth::check())
 						<li><a href="{{ url('/customers') }}">{{trans('menu.customers')}}</a></li>
@@ -56,7 +58,7 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
+						<li><a href="{{ url('/auth/login') }}">تسجيل الدخول</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
@@ -76,8 +78,8 @@
 
 	<footer class="footer hidden-print">
       <div class="container">
-        <p class="text-muted">You are using <a href="http://tutacare.github.io/tutapos">TutaPOS</a> v0.8-alpha by <a href="https://twitter.com/tutacare">Irfan Mahfudz Guntur</a>.
-        </p>
+       <!-- <p class="text-muted">You are using <a href="http://tutacare.github.io/tutapos">TutaPOS</a> v0.8-alpha by <a href="https://twitter.com/tutacare">Irfan Mahfudz Guntur</a>.
+        </p>-->
       </div>
     </footer>
 	<!-- Scripts -->
